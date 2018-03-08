6358b38cc0ce19521d921c0029bccd1ce4838f54
Kelley Robinson <krobinson@twilio.com>
2018-03-07 16:54:50 -0800

    Initial commit

---
+++ wthik.py
@@ -0,0 +1,78 @@
+from google.oauth2 import service_account
+from apiclient import discovery
+
+from flask import Flask, Response
+
+from datetime import datetime, timedelta
+
+
+app = Flask(__name__)
+app.config.from_object('app_config')
+
+
+def _get_credentials():
+    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+    SERVICE_ACCOUNT_FILE = 'service-secret.json'
+
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE,
+        scopes=SCOPES,
+        subject=app.config.get("SUBJECT"))
+
+    return credentials
+
+
+@app.route("/sms", methods=["GET", "POST"])
+def main():
+    credentials = _get_credentials()
+
+    service = discovery.build('calendar', 'v3', credentials=credentials)
+
+    now = datetime.utcnow()
+    today = now.isoformat() + 'Z'
+    tomorrow = (now + timedelta(days=1)).isoformat() + 'Z'
+
+    wthik_id = app.config.get("CALENDAR_ID")
+
+    # let google do the datetime math
+    currentEvent = service.events().list(
+        calendarId=wthik_id,
+        timeMin=today,
+        timeMax=tomorrow,
+        maxResults=1,
+        singleEvents=True,
+        orderBy='startTime'
+    ).execute()
+
+    currentlyTravelingTo = currentEvent.get('items', [])
+
+    if not currentlyTravelingTo:
+        futureEvents = service.events().list(
+            calendarId=wthik_id,
+            timeMin=today,
+            maxResults=1,
+            singleEvents=True,
+            orderBy='startTime'
+        ).execute()
+
+        event = futureEvents.get('items', [])[0]
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        msg = "Doesn't look like {} is currently traveling. Her next trip is to {} on {}".format(
+            app.config.get("TRAVELER"),
+            event['summary'],
+            start)
+    else:
+        event = currentlyTravelingTo[0]
+        end = event['end'].get('dateTime', event['end'].get('date'))
+        msg = "{} is currently in {} until {}".format(
+            app.config.get("TRAVELER"),
+            event['summary'],
+            end)
+
+    twiml = """<Response><Message>{}</Message></Response>""".format(msg)
+
+    return Response(twiml, mimetype="text/xml")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
