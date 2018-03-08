431809df082391e66ea7903e5ecc6cdc2e7b3c59
Kelley Robinson <krobinson@twilio.com>
2018-03-07 17:11:25 -0800

    update config to use environment vars for secret things

---
+++ app_config.py
@@ -0,0 +1,5 @@
+import os
+
+SUBJECT = os.environ["CALENDAR_SUBJECT"] # "myemail@mygoogleappsdomain.com"
+CALENDAR_ID = os.environ["CALENDAR_ID"] # "mycalendarid@group.calendar.google.com"
+TRAVELER = "Kelley"
